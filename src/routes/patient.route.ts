cbdea89df2646a4d1f8a6d79677b882eb23c3adc
M J <mohamedmujtaba0210@gmail.com>
2023-12-24 23:50:51 +0200

    feat: start patient controller

--- src/routes/patient.route.ts
+++ src/routes/patient.route.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { getPatients } from "../controllers/patient.controller";
 
 const router = Router();
+
+router.post("/", getPatients);
 
 export default router;
